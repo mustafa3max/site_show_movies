b43acc522ec3c48909fcccee7c558a4d3c38b807
mustafamax <abofatmaalslman@gmail.com>
2018-03-15 19:54:32 +0300

    initial commit

---
+++ Desktop/projects/movies_website/site.py
@@ -0,0 +1,7 @@
+import movie
+
+tomb_raider = movie.Movie('Tomb Raider', 'Lara Croft, the fiercely independent daughter of a missing adventurer, must push herself beyond her limits when she finds herself on the island where her father disappeared.', 'MV5BOTY4NDcyZGQtYmVlNy00ODgwLTljYTMtYzQ2OTE3NDhjODMwXkEyXkFqcGdeQXVyNzYzODM3Mzg@._V1_SY1000_CR0,0,674,1000_AL_.jpg', 'https://youtu.be/2w5VZmos5I4')
+black_panther = movie.Movie('Black Panther', 'A biologist signs up for a dangerous, secret expedition where the laws of nature don\'t apply.', 'https://ia.media-imdb.com/images/M', 'https://youtu.be/89OP78l9oF0')
+
+tomb_raider.show_trailer()
+black_panther.show_trailer()
